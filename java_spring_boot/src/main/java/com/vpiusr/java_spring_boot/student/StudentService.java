1106981dbdc8df52de802c731bd626f5f741e54c
Varun Pius Rodrigues <varunpius.rodrigues@nordstrom.com>
2021-05-13 22:58:46 -0700

    Initial addition

---
+++ java_spring_boot/src/main/java/com/vpiusr/java_spring_boot/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.vpiusr.java_spring_boot.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L, "Varun", "vpiusr@gmail.com",LocalDate.of(1990, 8, 21), 21)
+        );
+    }
+
+}
---
+++ java_spring_boot/src/test/java/com/vpiusr/java_spring_boot/JavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpiusr.java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
